3b6a07f2b939d48be5ccec5806b5671bb389ae61
redhaputra <putra.redha@gmail.com>
2017-10-11 15:46:45 +0700

    comment

--- routes/login.js
+++ routes/login.js
@@ -20,7 +20,7 @@ router.get('/login', (req, res) => {
 // 				res.redirect('/index')
 // 			}else{
 // 				res.render('login', {message :'password anda salah'})
-// 			}	
+// 			}
 // 		}else{
 // 			res.render('login', {message :'username anda belum terdaftar'})
 // 		}
@@ -44,34 +44,34 @@ router.post('/login', (req, res) =>{
 	})
 })
 
-router.get('/signup', (req, res) =>{
-	res.render('signup')
-})
+// router.get('/signup', (req, res) =>{
+// 	res.render('signup')
+// })
 
 router.get('/logout', (req, res, next)=>{
 	req.session.destroy()
 	res.redirect('/')
 })
 
-router.post('/signup', (req, res) =>{
-
-	let text ='' 
-    
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    
-    for(let i = 0; i < 8; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    let password = Enkrip(req.body.password, text)
-
-	Model.User.create({
-		username : req.body.username, 
-		password : password,
-		role : req.body.role,
-		createdAt : new Date(),
-    	updatedAt : new Date(),
-    	secret : text
-	}).then(res.redirect('/'))
-})
+// router.post('/signup', (req, res) =>{
+//
+// 	let text =''
+//
+//     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//
+//     for(let i = 0; i < 8; i++) {
+//         text += possible.charAt(Math.floor(Math.random() * possible.length));
+//     }
+//     let password = Enkrip(req.body.password, text)
+//
+// 	Model.User.create({
+// 		username : req.body.username,
+// 		password : password,
+// 		role : req.body.role,
+// 		createdAt : new Date(),
+//     	updatedAt : new Date(),
+//     	secret : text
+// 	}).then(res.redirect('/'))
+// })
 
 module.exports = router
